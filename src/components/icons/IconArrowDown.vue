0221be850d104731e6f2d34ce953858363a0417f
Nikita Dzemidovich <grodnonik2005@gmail.com>
2026-04-07 11:12:13 +0300

    chore(init): initialize vue 2 project

---
+++ src/components/icons/IconArrowDown.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M53.1209 23.8667L35.7342 41.2534C33.6809 43.3067 30.3209 43.3067 28.2675 41.2534L10.8809 23.8667"
+      stroke-width="4"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+import MainHeader from "@/components/MainHeader.vue";
+
+export default {
+  components: {
+    MainHeader,
+  },
+};
+</script>
+
+<template>
+  <MainHeader></MainHeader>
+</template>
+
+<style scoped></style>
